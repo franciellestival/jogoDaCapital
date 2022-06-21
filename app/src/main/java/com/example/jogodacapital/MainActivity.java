fd3bb09b7e8ac24527fed6886934ef91e94f00a7
Fran Mazzarotto <franciellestival@gmail.com>
2022-06-21 20:29:02 -0300

    first commit

---
+++ app/src/main/java/com/example/jogodacapital/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.jogodacapital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.HashMap;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+    Random r = new Random();
+
+    public void estadoCapital() {
+
+        capitalCities.put("Acre","Rio Branco");
+        capitalCities.put("Alagoas","Maceió");
+        capitalCities.put("Amapá","Macapá");
+        capitalCities.put("Amazonas","Manaus");
+        capitalCities.put("Bahia","Salvador");
+        capitalCities.put("Ceará","Fortaleza");
+        capitalCities.put("Espírito Santo","Vitória");
+        capitalCities.put("Goiás","Goiânia");
+        capitalCities.put("Maranhão","São Luís");
+        capitalCities.put("Mato Grosso","Cuiabá");
+        capitalCities.put("Mato Grosso do Sul","Campo Grande");
+        capitalCities.put("Minas Gerais","Belo Horizonte");
+        capitalCities.put("Pará","Belém");
+        capitalCities.put("Paraíba","João Pessoa");
+        capitalCities.put("Paraná","Curitiba");
+        capitalCities.put("Pernambuco","Recife");
+        capitalCities.put("Piauí","Teresina");
+        capitalCities.put("Rio de Janeiro","Rio de Janeiro");
+        capitalCities.put("Rio Grande do Norte","Natal");
+        capitalCities.put("Rio Grande do Sul","Porto Alegre");
+        capitalCities.put("Rondônia","Porto Velho");
+        capitalCities.put("Roraima","Boa Vista");
+        capitalCities.put("Santa Catarina","Florianópolis");
+        capitalCities.put("São Paulo","São Paulo");
+        capitalCities.put("Sergipe","Aracajú");
+        capitalCities.put("Tocantins","Palmas");
+        capitalCities.put("Distrito Federal","Brasília");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        estadoCapital();
+        String teste = capitalCities.get(r.nextInt(capitalCities.size()));
+
+        System.out.println(teste);
+    }
+}
